2c366134d3d230438bc455817da608a2b2a8fe0e
hukl <contact@smyck.org>
2012-04-15 18:19:10 +0200

    comment out all logging

--- apps/bitturret/src/bitturret_worker.erl
+++ apps/bitturret/src/bitturret_worker.erl
@@ -7,8 +7,8 @@
 
 % Matches connection requests by patternmatching on the 0
 handle( From, <<ConnectionID:64/big, 0:32/big, TransactionID:32/big, _Rest/binary>>) ->
-    error_logger:info_msg("Connection Request"),
-    error_logger:info_msg("C: ~p A: ~p T: ~p~n", [ConnectionID, 0, TransactionID]),
+    % error_logger:info_msg("Connection Request"),
+    % error_logger:info_msg("C: ~p A: ~p T: ~p~n", [ConnectionID, 0, TransactionID]),
     Response = <<0:32/big, TransactionID:32/big, ?CONNECTION_ID:64/big>>,
     send_response( From, Response );
 
@@ -32,21 +32,21 @@ handle(From = {_, PeerIP, _},
         ) ->
 
     case Event of
-        0 ->
-            error_logger:info_msg("Announce Request: Regular");
-        1 ->
-            error_logger:info_msg("Announce Request: Completed");
+        0 -> ok;
+            % error_logger:info_msg("Announce Request: Regular");
+        1 -> ok;
+            % error_logger:info_msg("Announce Request: Completed");
         2 ->
-            error_logger:info_msg("Announce Request: Started"),
+            % error_logger:info_msg("Announce Request: Started"),
             ets:insert(peers, {InfoHash, ip_to_int(PeerIP), PeerPort, leecher});
         3 ->
-            error_logger:info_msg("Announce Request: Stopped"),
+            % error_logger:info_msg("Announce Request: Stopped"),
             ets:match_delete(peers, {InfoHash, ip_to_int(PeerIP), PeerPort, '_'})
     end,
 
     Peers    = ets:match(peers, {InfoHash,'$1','$2','_'} ),
 
-    error_logger:info_msg("Peers: ~p~n", [Peers]),
+    % error_logger:info_msg("Peers: ~p~n", [Peers]),
 
     PeersBin = << <<IP:32/big,Port:16/big>> || [IP,Port] <- Peers >>,
     Response = <<1:32/big, TransactionID:32/big, 2160:32/big, 10:32/big, 5:32/big, PeersBin/binary>>,
@@ -54,11 +54,11 @@ handle(From = {_, PeerIP, _},
 
 % Matches scrape requests by patternmatching on the 2
 handle(From, <<?CONNECTION_ID:64/big, 2:32/big, TransactionID:32/big, Rest/binary>>) ->
-    error_logger:info_msg("Scrape Request: ~n"),
+    % error_logger:info_msg("Scrape Request: ~n"),
     Resp = <<2:32/big, TransactionID:32/big, 1:32/big, 3:32/big, 10:32/big>>;
 
 handle(From, Msg) ->
-    error_logger:info_msg("Something else: ~p~n", [Msg]).
+    % error_logger:info_msg("Something else: ~p~n", [Msg]).
 
 send_response({Socket, IP, Port}, Response) ->
     gen_udp:send(Socket, IP, Port, Response).
