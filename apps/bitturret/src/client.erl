6155e4d59bcd98a1e53906de9eb4c06922f388ef
Johannes Huning <mail@johanneshuning.com>
2012-04-17 16:43:06 +0200

    Test client and buffer strategy

---
+++ apps/bitturret/src/client.erl
@@ -0,0 +1,36 @@
+-module (client).
+-compile (export_all).
+
+-define (MSG,
+    <<12,92,95,245,51,42,96,72,0,0,0,1,131,67,180,190,154,53,107,2,128,232,204,
+      243,211,136,180,87,0,160,158,170,70,77,130,249,45,84,82,50,53,49,48,45,99,
+      99,102,111,110,56,52,54,106,107,103,108,0,0,0,0,0,0,0,0,0,0,0,0,6,26,128,
+      0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,49,123,193,195,0,0,0,80,233,194>>).
+
+start(LocalIp, LocalPort, RemoteIp, NumPackets) ->
+    {ok, Socket} = gen_udp:open(LocalPort, [binary, {ip, LocalIp}, {active, false}]),
+    send(Socket, RemoteIp, NumPackets).
+
+
+
+send(Socket, RemoteIp, NumPackets) ->
+    {Dur, _} = timer:tc(fun() ->
+        times(NumPackets, fun() ->
+            gen_udp:send(Socket, RemoteIp, 1259, ?MSG)
+        end)
+    end),
+    SleepDur = erlang:trunc(1000 - (Dur / 1000)),
+    if
+        SleepDur < 0 ->
+            throw({error, lame_client});
+        true ->
+            timer:sleep(SleepDur),
+            send(Socket, RemoteIp, NumPackets)
+    end.
+
+
+times(0, _Fun) -> ok;
+
+times(Times, Fun) ->
+    Fun(),
+    times(Times - 1, Fun).
