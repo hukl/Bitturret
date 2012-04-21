aa042adf355bbf87b698a089ae97db819842f8a0
Johannes Huning <mail@johanneshuning.com>
2012-04-21 23:42:35 +0200

    Remove management API from bitturret_handler
    It will already be part of a supervision tree inside the application

--- apps/bitturret/src/bitturret_handler.erl
+++ apps/bitturret/src/bitturret_handler.erl
@@ -1,9 +1,6 @@
 -module(bitturret_handler).
 -behavior(gen_server).
 
-% Managment API
--export([start/0, start_link/0, stop/0]).
-
 % gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
          code_change/3]).
@@ -12,18 +9,6 @@
 -export([]).
 
 -record(state, { socket }).
-
-%% ===================================================================
-%% Management API
-%% ===================================================================
-
-start() ->
-    gen_server:start( {local, ?MODULE}, ?MODULE, [], []).
-
-start_link() ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
-
-stop() -> gen_server:cast(?MODULE, stop).
 
 %% ===================================================================
 %% gen_server callbacks
