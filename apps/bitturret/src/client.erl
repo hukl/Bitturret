38f07605aa9e27f0dcc50ccf2144aeb606dd6596
Johannes Huning <mail@johanneshuning.com>
2012-04-22 02:19:26 +0200

    Make sure the client blows up on errors

--- apps/bitturret/src/client.erl
+++ apps/bitturret/src/client.erl
@@ -19,7 +19,7 @@ start(LocalIp, LocalPort, RemoteIp, RemotePort, PacketsPerSec) ->
 loop_send(Socket, RemoteIp, RemotePort, PacketsPerSec) ->
     {Dur, _} = timer:tc(fun() ->
         times(PacketsPerSec, fun() ->
-            gen_udp:send(Socket, RemoteIp, RemotePort, ?MSG)
+            ok = gen_udp:send(Socket, RemoteIp, RemotePort, ?MSG)
         end)
     end),
     SleepDur = erlang:trunc(1000 - (Dur / 1000)),
